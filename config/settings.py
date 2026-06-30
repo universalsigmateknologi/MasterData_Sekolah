de7c35956acc59d5e29a7990e162234c5690a41d
universalsigmateknologi <universalsigmateknologi@gmail.com>
2026-06-30 09:09:02 +0700

    mantap

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'akademik',
     'siswa',
+    'accounts',
+    'pengaturan',
     'dashboard',
     'auth.apps.CustomAuthConfig',
     'kesiswaan',
