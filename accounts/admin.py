ffbcce601619257fb418170e230972c822668e83
universalsigmateknologi <universalsigmateknologi@gmail.com>
2026-06-30 10:17:28 +0700

    mantap

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Pengaturan
 
-# Register your models here.
+@admin.register(Pengaturan)
+class PengaturanAdmin(admin.ModelAdmin):
+    list_display = ('nama_program', 'updated_at')
+    
+    def has_add_permission(self, request):
+        """Menyembunyikan tombol 'Tambah' jika sudah ada data"""
+        if Pengaturan.objects.exists():
+            return False
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        """(Opsional) Mencegah pengaturan dihapus agar sistem tidak error"""
+        return False
